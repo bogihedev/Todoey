3b15294608e8faad7605b2dcb288be20db20b9a4
Bogi Eliasen <bogidev05@gmail.com>
2019-04-15 22:41:55 +0100

    Convert to MVC design pattern

---
+++ Todoey/Data Model/Item.swift
@@ -0,0 +1,14 @@
+//
+//  Item.swift
+//  Todoey
+//
+//  Created by Bogi Eliasen on 4/15/19.
+//  Copyright © 2019 Bogi Eliasen. All rights reserved.
+//
+
+import Foundation
+
+class Item {
+  var title : String = ""
+  var done : Bool = false
+}
